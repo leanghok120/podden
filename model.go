e903d493a3f618b7a2e00b9fae9fc8a0320b0abb
leanghok <leanghokoeng5@gmail.com>
2025-09-14 07:13:18 +0700

    remove testCover

--- model.go
+++ model.go
@@ -239,8 +239,6 @@ func (m model) View() string {
 		timeInfo := timeStyle.Render(fmt.Sprintf("%s / %s", m.elapsed, m.total))
 		lyric := lyricStyle.Render(m.currLyric)
 
-		testCover("/home/leanghok/Pictures/121.png")
-
 		playingContent := lipgloss.JoinVertical(
 			lipgloss.Left,
 			title,
